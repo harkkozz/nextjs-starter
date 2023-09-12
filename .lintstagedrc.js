2e91c48c055c150c1aeaa6099918acdca96cdb41
harkkozz <adilovicharis94@gmail.com>
2023-09-12 16:35:19 +0200

    chore: add husky shell and github workflow

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
-
-module.exports = {
-  '**/*.{ts,tsx,css,md}': 'prettier --write',
-  '**/*.ts?(x)': [buildEslintCommand, 'jest --bail --onlyChanged --passWithNoTests'],
-};
